944f2d7d8e6026fcd38fdbfa0cec8579f09940a3
SergioPerez <dark07perez@gmail.com>
2018-08-12 11:35:05 -0500

    solucionando impresion tabla

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -5,21 +5,20 @@
   require '../../controller/database.php';
   
   if (isset($_SESSION['user_id'])) {
-    $query = "SELECT id, email, password FROM testudiantes WHERE id ='".$_SESSION['user_id']."'";
-    $consul = mysqli_query($conn, $query);
-    $results = mysqli_fetch_array($consul);
+    $query_u = "SELECT * FROM testudiantes WHERE id ='".$_SESSION['user_id']."'";
+    $consul_u = mysqli_query($conn, $query_u);
+    $results_u = mysqli_fetch_array($consul_u);
   }
   else{
     header('Location: ../login.php');  
   }
-
   $user = null; 
-  
-  if (count($results) > 0) {
-    $user = $results;
+
+  if (count($results_u) > 0) {
+    $user = $results_u;
   } 
 
-  $message = "<h4> Sesión iniciada como: "."<b>".$user["email"]."</b>"."</h4  >";
+  $message = "<h4> Sesión iniciada como: ".$user["nombre"]." ".$user["apellido"]."</h4>";
 
 ?>
 
@@ -44,30 +43,38 @@
     <!-- img src="../../media/logo.png" width="384,4" height="151,5" alt="Escuela Colombiana de Ingeniería Julio Garavito" align="middle" -->
     <div id = "tabla">
     
-    <?php
-    
-    $query = "SELECT thorarios.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= 'mama'";
-    $consul = mysqli_query($conn, $query);
-    $results = mysqli_fetch_array($consul);
+      <?php 
 
-    
-    echo'<table class="gridtable" align="center">'; 
-      echo'<tr>'; 
-        echo '<th>'."Estudiante".'</th>';
-        echo '<th>'."Día 1".'</th>';
-        echo '<th>'."Día 2".'</th>';
-        echo '<th>'."Día 2".'</th>';
-      echo'</tr>'; 
-      echo'<tr>'; 
-        echo '<td>'.$results['email'].'</td>';
-        echo '<td>'.$results['dia1'].'<br>'.$results['hora1'].'</td>';
-        echo '<td>'.$results['dia2'].'<br>'.$results['hora2'].'</td>';
-        echo '<td>'.$results['dia3'].'<br>'.$results['hora3'].'</td>';        
-      echo'</tr>';
-    echo'</table>';
+      $query = "SELECT done FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
+      $consul = mysqli_query($conn, $query);
+      $results = mysqli_fetch_array($consul);
       
-    ?>
-   
+      if($results["done"]==0){ # si es 0 es porque no ha hecho horario (false)
+        $query = "SELECT thorarios.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
+        $consul = mysqli_query($conn, $query);
+        $results = mysqli_fetch_array($consul);
+
+        
+        echo'<table class="gridtable" align="center">'; 
+          echo'<tr>'; 
+            echo '<th>'."Estudiante".'</th>';
+            echo '<th>'."Día 1".'</th>';
+            echo '<th>'."Día 2".'</th>';
+            echo '<th>'."Día 2".'</th>';
+          echo'</tr>'; 
+          echo'<tr>'; 
+            echo '<td>'.$results['email'].'</td>';
+            echo '<td>'.$results['dia1'].'<br>'.$results['hora1'].'</td>';
+            echo '<td>'.$results['dia2'].'<br>'.$results['hora2'].'</td>';
+            echo '<td>'.$results['dia3'].'<br>'.$results['hora3'].'</td>';        
+          echo'</tr>';
+        echo'</table>';
+        
+      }
+
+            
+      ?>
+    
    
     </div>
 
