d65e265b632b3e6ca7c3e73605ae2620969919f9
SergioPerez <dark07perez@gmail.com>
2018-08-13 03:43:48 -0500

    sin tabla comentada vieja antigua

--- pages/partials/header.php
+++ pages/partials/header.php
@@ -70,134 +70,99 @@
    <br>
    <br>
    <br>
-    
-    <!-- img src="../../media/logo.png" width="384,4" height="151,5" alt="Escuela Colombiana de Ingeniería Julio Garavito" align="middle" -->
-    <div id = "tabla">
-    
-      <?php 
-      /*
-      $query = "SELECT done FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
-      $consul = mysqli_query($conn, $query);
-      $results = mysqli_fetch_array($consul);
-      
-      if($results["done"]==1){ # si es 1 es porque ya hizo horario (true)
-        $query = "SELECT testudiantes.codigo as codigo,  thorarios.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
-        $consul = mysqli_query($conn, $query);
-        $results = mysqli_fetch_array($consul);
 
-        
-        '<table class="gridtable" align="center">'; 
-          '<tr>'; 
-             '<th>'."Estudiante".'</th>';
-             '<th>'."Día 1".'</th>';
-             '<th>'."Día 2".'</th>';
-             '<th>'."Día 3".'</th>';
-          '</tr>'; 
-          '<tr>'; 
-             '<td>'.$results['codigo'].'</td>';
-             '<td>'.$results['dia1'].'<br>'.$results['hora1'].'</td>';
-             '<td>'.$results['dia2'].'<br>'.$results['hora2'].'</td>';
-             '<td>'.$results['dia3'].'<br>'.$results['hora3'].'</td>';        
-          '</tr>';
-        '</table>';  
-       
-
-      }else{     
-      
+    <div id = "tabla">  
       
 
-      }    */
-      ?>
+      <?php if(!empty($message_h)): ?>
+        <p> <?= $message_h ?></p>
+      <?php endif; ?>
 
-    <?php if(!empty($message_h)): ?>
-      <p> <?= $message_h ?></p>
-    <?php endif; ?>
+      <?php
+      $query_d = "SELECT dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
+      $consul_d = mysqli_query($conn, $query_d);
+      $results_d = mysqli_fetch_array($consul_d);
 
-    <?php
-    $query_d = "SELECT dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
-    $consul_d = mysqli_query($conn, $query_d);
-    $results_d = mysqli_fetch_array($consul_d);
-
-    
-    echo '<form name="mod_horario" action="header.php" method="post">';
-    echo '    Día1: <select name="dia1">';
-    echo '            <option selected="'.$results_d['dia1'].'" >'.$results_d['dia1'].'</option>                ';
-    echo '            <option value="ninguno">N/A</option>';
-    echo '            <option value="Lunes">Lunes</option>';
-    echo '            <option value="Martes">Martes</option>';
-    echo '            <option value="Miércoles">Miércoles</option>';
-    echo '            <option value="Jueves">Jueves</option>';
-    echo '            <option value="Viernes">Viernes</option>';
-    echo '            <option value="Sábado">Sábado</option>                ';
-    echo '            </select>';
-    echo '    Hora día 1: <select name="hora1">';
-    echo '            <option selected="'.$results_d['hora1'].'" >'.$results_d['hora1'].'</option>                ';
-    echo '            <option value="7:00">7:00</option>';
-    echo '            <option value="8:00">8:00</option>';
-    echo '            <option value="9:00">9:00</option>';
-    echo '            <option value="10:00">10:00</option>';
-    echo '            <option value="11:00">11:00</option>';
-    echo '            <option value="12:00">12:00</option>';
-    echo '            <option value="13:00">13:00</option>';
-    echo '            <option value="14:00">14:00</option>';
-    echo '            <option value="15:00">15:00</option>';
-    echo '            <option value="16:00">16:00</option>';
-    echo '            <option value="17:00">17:00</option>';
-    echo '            </select>';
-    echo '    <br><br>      ';
-    echo '    Día2: <select name="dia2">';
-    echo '            <option selected="'.$results_d['dia2'].'" >'.$results_d['dia2'].'</option>                ';
-    echo '            <option value="Lunes">Lunes</option>';
-    echo '            <option value="Martes">Martes</option>';
-    echo '            <option value="Miércoles">Miércoles</option>';
-    echo '            <option value="Jueves">Jueves</option>';
-    echo '            <option value="Viernes">Viernes</option>';
-    echo '            <option value="Sábado">Sábado</option>                ';
-    echo '            </select>';
-    echo '          ';
-    echo '    ';
-    echo '    Hora día 2: <select name="hora2">';
-    echo '            <option selected="'.$results_d['hora2'].'" >'.$results_d['hora2'].'</option>                ';
-    echo '            <option value="7:00">7:00</option>';
-    echo '            <option value="8:00">8:00</option>';
-    echo '            <option value="9:00">9:00</option>';
-    echo '            <option value="10:00">10:00</option>';
-    echo '            <option value="11:00">11:00</option>';
-    echo '            <option value="12:00">12:00</option>';
-    echo '            <option value="13:00">13:00</option>';
-    echo '            <option value="14:00">14:00</option>';
-    echo '            <option value="15:00">15:00</option>';
-    echo '            <option value="16:00">16:00</option>';
-    echo '            <option value="17:00">17:00</option>';
-    echo '            </select>';
-    echo '    <br><br>      ';
-    echo '    Día3: <select name="dia3">';
-    echo '            <option selected="'.$results_d['dia3'].'" >'.$results_d['dia3'].'</option>                ';
-    echo '            <option value="Lunes">Lunes</option>';
-    echo '            <option value="Martes">Martes</option>';
-    echo '            <option value="Miércoles">Miércoles</option>';
-    echo '            <option value="Jueves">Jueves</option>';
-    echo '            <option value="Viernes">Viernes</option>';
-    echo '            <option value="Sábado">Sábado</option>                ';
-    echo '            </select>               ';
-    echo '    ';
-    echo '    Hora día 3: <select name="hora3">';
-    echo '            <option selected="'.$results_d['hora3'].'" >'.$results_d['hora3'].'</option>                ';
-    echo '            <option value="7:00">7:00</option>';
-    echo '            <option value="8:00">8:00</option>';
-    echo '            <option value="9:00">9:00</option>';
-    echo '            <option value="10:00">10:00</option>';
-    echo '            <option value="11:00">11:00</option>';
-    echo '            <option value="12:00">12:00</option>';
-    echo '            <option value="13:00">13:00</option>';
-    echo '            <option value="14:00">14:00</option>';
-    echo '            <option value="15:00">15:00</option>';
-    echo '            <option value="16:00">16:00</option>';
-    echo '            <option value="17:00">17:00</option>';
-    echo '            </select>        ';
-    echo '    <br>';
-    echo '  <input type="submit" name="submit" />';
-    echo '  </form>';
+      
+      echo '<form name="mod_horario" action="header.php" method="post">';
+      echo '    Día1: <select name="dia1">';
+      echo '            <option selected="'.$results_d['dia1'].'" >'.$results_d['dia1'].'</option>                ';
+      echo '            <option value="ninguno">N/A</option>';
+      echo '            <option value="Lunes">Lunes</option>';
+      echo '            <option value="Martes">Martes</option>';
+      echo '            <option value="Miércoles">Miércoles</option>';
+      echo '            <option value="Jueves">Jueves</option>';
+      echo '            <option value="Viernes">Viernes</option>';
+      echo '            <option value="Sábado">Sábado</option>                ';
+      echo '            </select>';
+      echo '    Hora día 1: <select name="hora1">';
+      echo '            <option selected="'.$results_d['hora1'].'" >'.$results_d['hora1'].'</option>                ';
+      echo '            <option value="7:00">7:00</option>';
+      echo '            <option value="8:00">8:00</option>';
+      echo '            <option value="9:00">9:00</option>';
+      echo '            <option value="10:00">10:00</option>';
+      echo '            <option value="11:00">11:00</option>';
+      echo '            <option value="12:00">12:00</option>';
+      echo '            <option value="13:00">13:00</option>';
+      echo '            <option value="14:00">14:00</option>';
+      echo '            <option value="15:00">15:00</option>';
+      echo '            <option value="16:00">16:00</option>';
+      echo '            <option value="17:00">17:00</option>';
+      echo '            </select>';
+      echo '    <br><br>      ';
+      echo '    Día2: <select name="dia2">';
+      echo '            <option selected="'.$results_d['dia2'].'" >'.$results_d['dia2'].'</option>                ';
+      echo '            <option value="Lunes">Lunes</option>';
+      echo '            <option value="Martes">Martes</option>';
+      echo '            <option value="Miércoles">Miércoles</option>';
+      echo '            <option value="Jueves">Jueves</option>';
+      echo '            <option value="Viernes">Viernes</option>';
+      echo '            <option value="Sábado">Sábado</option>                ';
+      echo '            </select>';
+      echo '          ';
+      echo '    ';
+      echo '    Hora día 2: <select name="hora2">';
+      echo '            <option selected="'.$results_d['hora2'].'" >'.$results_d['hora2'].'</option>                ';
+      echo '            <option value="7:00">7:00</option>';
+      echo '            <option value="8:00">8:00</option>';
+      echo '            <option value="9:00">9:00</option>';
+      echo '            <option value="10:00">10:00</option>';
+      echo '            <option value="11:00">11:00</option>';
+      echo '            <option value="12:00">12:00</option>';
+      echo '            <option value="13:00">13:00</option>';
+      echo '            <option value="14:00">14:00</option>';
+      echo '            <option value="15:00">15:00</option>';
+      echo '            <option value="16:00">16:00</option>';
+      echo '            <option value="17:00">17:00</option>';
+      echo '            </select>';
+      echo '    <br><br>      ';
+      echo '    Día3: <select name="dia3">';
+      echo '            <option selected="'.$results_d['dia3'].'" >'.$results_d['dia3'].'</option>                ';
+      echo '            <option value="Lunes">Lunes</option>';
+      echo '            <option value="Martes">Martes</option>';
+      echo '            <option value="Miércoles">Miércoles</option>';
+      echo '            <option value="Jueves">Jueves</option>';
+      echo '            <option value="Viernes">Viernes</option>';
+      echo '            <option value="Sábado">Sábado</option>                ';
+      echo '            </select>               ';
+      echo '    ';
+      echo '    Hora día 3: <select name="hora3">';
+      echo '            <option selected="'.$results_d['hora3'].'" >'.$results_d['hora3'].'</option>                ';
+      echo '            <option value="7:00">7:00</option>';
+      echo '            <option value="8:00">8:00</option>';
+      echo '            <option value="9:00">9:00</option>';
+      echo '            <option value="10:00">10:00</option>';
+      echo '            <option value="11:00">11:00</option>';
+      echo '            <option value="12:00">12:00</option>';
+      echo '            <option value="13:00">13:00</option>';
+      echo '            <option value="14:00">14:00</option>';
+      echo '            <option value="15:00">15:00</option>';
+      echo '            <option value="16:00">16:00</option>';
+      echo '            <option value="17:00">17:00</option>';
+      echo '            </select>        ';
+      echo '    <br>';
+      echo '  <input type="submit" name="submit" />';
+      echo '  </form>';
       ?>
 
     </div>
