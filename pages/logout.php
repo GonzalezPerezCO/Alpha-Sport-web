3fd7490fd25375d848c5824172172c52694c88da
SergioPerez <dark07perez@gmail.com>
2018-08-15 09:46:04 -0500

    solucionando cerar sesion no funcionaba

--- pages/logout.php
+++ pages/logout.php
@@ -19,7 +19,20 @@
   unset($_COOKIE['user_hora2']);
   unset($_COOKIE['user_hora3']);
 
-  header('Location: ../index.php');
+
+  setcookie('user_id', null, -1);
+  setcookie('user_email', null, -1);
+  setcookie('user_nombre', null, -1);
+  setcookie('user_apellido', null, -1);
+  setcookie('user_dia1', null, -1);
+  setcookie('user_dia2', null, -1);
+  setcookie('user_dia3', null, -1);
+  setcookie('user_hora1', null, -1);
+  setcookie('user_hora2', null, -1);
+  setcookie('user_hora3', null, -1);
+
+
+  #header('Location: ../index.php');
 
 
 
