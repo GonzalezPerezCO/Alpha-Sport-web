abe507c6ce45af0c200f43baea72a65a24d972b1
SergioPerez <dark07perez@gmail.com>
2018-08-16 02:25:10 -0500

    probando pq no funciona

--- pages/partials/horario.php
+++ pages/partials/horario.php
@@ -17,9 +17,9 @@
 
   $message_h="Sin cambios";
 
-if ($_POST['dia1']!="N/A" || $_POST['dia2']!="N/A" || $_POST['dia2']!="N/A" ) {  
-    $message_h="Solicitud realizada";
-
+if ($_POST['dia1']!="N/A" || $_POST['dia2']!="N/A" || $_POST['dia2']!="N/A" ) {
+    $message_h="Solicitud realizada";    
+    
     #$query_d = "SELECT dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$user["email"]."'";
     #$consul_d = mysqli_query($conn, $query_d) or die(mysqli_error($conn));
     #$results_d = mysqli_fetch_array($consul_d);
@@ -31,11 +31,11 @@ if ($_POST['dia1']!="N/A" || $_POST['dia2']!="N/A" || $_POST['dia2']!="N/A" ) {
     #$hora2=$results_d["hora2"];
     #$hora3=$results_d["hora3"];
 
-    # Elimar un día, por consiguiente elimina una hora
+    # Eliminar un día/hora, por consiguiente elimina una hora/día
     if($_POST['dia1']=="N/A" || $_POST['hora1'=="N/A"]){
       $query_u = "UPDATE thorarios SET dia1='N/A', hora1='N/A' WHERE email='".$_COOKIE['email']."'";
       $consul_u= mysqli_query($conn, $query_u) or die(mysqli_error($conn));
-          
+    
     }elseif($_POST['dia2']=="N/A" || $_POST['hora2'=="N/A"]){
       $query_u = "UPDATE thorarios SET dia2='N/A', hora2='N/A' WHERE email='".$_COOKIE['email']."'";
       $consul_u= mysqli_query($conn, $query_u) or die(mysqli_error($conn));
@@ -51,6 +51,5 @@ if ($_POST['dia1']!="N/A" || $_POST['dia2']!="N/A" || $_POST['dia2']!="N/A" ) {
   else{
     $message_h="Tiene que inscribir por lo menos un día y una hora";
   }
-
   header('Location: header.php');
 ?>
