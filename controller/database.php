71c0e6302f986d7275489c059f2af84f03b8278f
SergioPerez <dark07perez@gmail.com>
2018-08-17 07:00:23 -0500

    uri

--- controller/database.php
+++ controller/database.php
@@ -1,14 +1,16 @@
 <?php
 
-$actual_link = "http://$_SERVER[HTTP_HOST]";
+$actual_link = $_SERVER[HTTP_HOST];
+$uri = $_SERVER['REQUEST_URI'];
 
-$server = str_replace("http://","",$actual_link);
+$server = $actual_link;
 $username = 'deportes';
 $password = 'deportes20182';
 $database = 'deportes';
 
 $conn = mysqli_connect($server, $username, $password, $database) or die(mysqli_error($conn));
 
-echo $actual_link;
+echo $actual_link.", ".$uri;
+
 
 ?>
