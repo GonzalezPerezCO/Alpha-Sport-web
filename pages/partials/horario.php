0ebfd21d4ed05d058a3f197d7d206c77430121fa
SergioPerez <dark07perez@gmail.com>
2018-08-17 10:36:42 -0500

    limpio

--- pages/partials/horario.php
+++ pages/partials/horario.php
@@ -20,27 +20,6 @@
   $ADATA['user_hora2']= $results["hora2"];
   $ADATA['user_hora3']= $results["hora3"];
 
-  /*echo $ADATA['user_nombre'];
-  echo $ADATA['user_apellido'];
-  echo $ADATA['user_email'];
-  echo $ADATA['user_dia1'];
-  echo $ADATA['user_dia2'];
-  echo $ADATA['user_dia3'];
-  echo $ADATA['user_hora1'];
-  echo $ADATA['user_hora2'];
-  echo $ADATA['user_hora3'];
-  echo "<br>";
-  echo $_POST['dia1'];
-  echo $_POST['dia2'];
-  echo $_POST['dia3'];
-  echo $_POST['hora1'];
-  echo $_POST['hora2'];
-  echo $_POST['hora3'];*/
-
-
-  #$message_h=" Sin cambios";
-
-  $message_h=" Solicitud realizada ";    
 
   /* CRITERIOS PARA MANEJAS EVENTOS DE POST
     --------------------------------------------------------------------
@@ -48,63 +27,9 @@
     2. Caso por días
     3. Solo hay cambios si POST es diferente a lo que esta en COOKIE
     4. N/A para un día/hora, por consiguiente N/A para una hora/día    
-    -------------------------------------------------------------------- */
-    
-  #if($_POST['dia1'] == $_POST['dia2'] || $_POST['dia1'] == $_POST['dia3'] || $_POST['dia3'] == $_POST['dia2']){
-    if(false){
-
-    $message_h=" Hay días repetidos, no es posible asignar mas de una franja en un solo día ";
-  }else{
-
-    # PARA DIA 1
-  if($_POST['dia1']!=$ADATA['user_dia1'] || $_POST['hora1']!=$ADATA['user_hora1'] ){      
-    if($_POST['dia1']=="N/A" || $_POST['hora1'] =="N/A"){
-      $query = "UPDATE thorarios SET dia1='N/A', hora1='N/A' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-      $message_h=$message_h." | Eliminado dia 1 | ";
-    }else{
-      $query = "UPDATE thorarios SET dia1='".$_POST['dia1']."', hora1='".$_POST['hora1']."' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-      $message_h=$message_h." | Modificado dia 1 | ";
-    }
-
-
-  }
+    -------------------------------------------------------------------- */   
   
-  # PARA DIA 2
-  if($_POST['dia2']!=$ADATA['user_dia2'] || $_POST['hora2']!=$ADATA['user_hora2'] ){      
-    if($_POST['dia2']=="N/A" || $_POST['hora2'] =="N/A"){
-      $query = "UPDATE thorarios SET dia2='N/A', hora2='N/A' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
 
-      $message_h=$message_h." | Eliminado dia 2 | ";    
-    }else{
-      $query = "UPDATE thorarios SET dia2='".$_POST['dia2']."', hora2='".$_POST['hora2']."' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
 
-      $message_h=$message_h." | Modificado dia 2 | ";
-    }
-
-  }
-  
-  # PARA DIA 3
-  if($_POST['dia3']!=$ADATA['user_dia3'] || $_POST['hora3']!=$ADATA['user_hora3'] ){      
-    if($_POST['dia3']=="N/A" || $_POST['hora3'] =="N/A"){
-      $query = "UPDATE thorarios SET dia3='N/A', hora3='N/A' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-      $message_h=$message_h." | Eliminado dia 3 | ";
-    }else{
-      $query = "UPDATE thorarios SET dia3='".$_POST['dia3']."', hora3='".$_POST['hora3']."' WHERE email='".$ADATA['user_email']."'";
-      $consul= mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-      $message_h=$message_h." | Modificado dia 3 | ";
-    }
-    
-  }
-
-  }  
   header('Location: header.php');
 ?>
