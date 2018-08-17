181c3136f34399c2fd0245a96b3f1a2a323806db
SergioPerez <dark07perez@gmail.com>
2018-08-17 10:06:08 -0500

    ajuestes adata con sql

--- pages/partials/horario.php
+++ pages/partials/horario.php
@@ -7,6 +7,20 @@
     header('Location: ../login.php');
   }  
 
+  $query = "SELECT testudiantes.nombre as nombre, testudiantes.apellido as apellido, testudiantes.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE testudiantes.email = thorarios.email AND testudiantes.id= '".$_COOKIE['user_id']."'";
+  $consul = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  $results = mysqli_fetch_array($consul);
+
+  $ADATA['user_nombre']= $results["nombre"];
+  $ADATA['user_apellido']= $results["apellido"];
+  $ADATA['user_email']= $results["email"];
+  $ADATA['user_dia1']= $results["dia1"];
+  $ADATA['user_dia2']= $results["dia2"];
+  $ADATA['user_dia3']= $results["dia3"];
+  $ADATA['user_hora1']= $results["hora1"];
+  $ADATA['user_hora2']= $results["hora2"];
+  $ADATA['user_hora3']= $results["hora3"];
+
   #$message_h=" Sin cambios";
 
   $message_h=" Solicitud realizada ";    
@@ -78,12 +92,12 @@
   #$message_h=" <font color='red'> <b>Tiene que inscribir por lo menos un día y una hora</b> </font>";
 
 
-  $query = "SELECT testudiantes.id as id, testudiantes.nombre as nombre, testudiantes.apellido as apellido, testudiantes.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$_POST["email"]."' AND password = '".$_POST['password']."'";
-  $consul = mysqli_query($conn, $query) or die(mysqli_error($conn));
-  $results = mysqli_fetch_array($consul);
+  #$query = "SELECT testudiantes.id as id, testudiantes.nombre as nombre, testudiantes.apellido as apellido, testudiantes.email as email, dia1, dia2, dia3, hora1, hora2, hora3 FROM thorarios INNER JOIN testudiantes ON thorarios.email = testudiantes.email WHERE thorarios.email= '".$_POST["email"]."' AND password = '".$_POST['password']."'";
+  #$consul = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  #$results = mysqli_fetch_array($consul);
    
 
-  $tiempo_cook=time()+900; // tiempo
+  #$tiempo_cook=time()+900; // tiempo
 
   #setcookie('user_id', $results["id"], $tiempo_cook, "/");
   #setcookie('user_email', $results["email"], $tiempo_cook, "/");
