cf2f750eb488e79876598f440e827ac16500736c
SergioPerez <dark07perez@gmail.com>
2018-08-18 02:09:19 -0500

    ajuestes vista

--- index.php
+++ index.php
@@ -39,30 +39,28 @@
         <h3>Por favor ingrese o regístrese</h3>
         <a href="pages/login.php">Ingresar</a> <br>
         o
-        <br><a href="pages/signup.php">Registrase</a>
+        <br><a href="pages/signup.php">Registrarse</a>
       <?php endif; ?>
     
       <p>
       <b>Instrucciones:</b><br>
-      Sí necesita ayuda con el sistema o encuentra errores, puede comunicarse a los correos de los desarrolladores que se encuentra en la parte inferior de esta página,
-      Primero registrese en <b>Registrase</b> con su número de reserva y complete los datos que allí se le piden. Para hacer el registro lo puede hacer en cualquier momento de día
+      Sí necesita ayuda con el sistema, puede comunicarse a los correos de los desarrolladores que verá en la parte inferior,
+      Primero registrese en <b>Registrarse</b> con su número de reserva y complete los datos que se le piden. El registro lo puede hacer en cualquier momento de día
       hoy Agosto 18.
       </p>
       <p>
-      Despues de crear su cuenta en <b>Registrase</b> inicie sesión con el correo y la contraseña que registro. Luego de esto puede hacer su horario.      
+      Despues de crear su cuenta inicie sesión. Luego de esto puede hacer su horario.      
       </p>
       <p>
-      <b>Recuerde que tiene 10 minutos para hacer su horario desde la hora que le corresponde inscribir.</b>   
+      <b>Recuerde que tiene 10 minutos para hacer su horario.</b>   
       </p>
 
       <p class="desc">
         Esta es una <a href=""> iniciativa </a> propia con el apoyo del <a href="http://laboratorio.is.escuelaing.edu.co/"> Laboratorio de Sistemas </a>
       y <a href="https://www.escuelaing.edu.co/es/vida_universitaria/bienestar_universitario/recreacion/programa"> Bienestar Universitario</a>, para la elaboración de horarios de los estudiantes al Gimnasio de la Escuela.
-      </p>
-      <p>
       Para saber mas visite<a href="http://notiweb.escuelaing.edu.co/blog/2018/07/24/ganadores-de-la-trigesima-sexta-version-de-la-vitrina-academica-2018-i/"> el NOTIWEB. </a>
       </p>
-    </section>
+      </section>
 
     <footer>
       <p>
