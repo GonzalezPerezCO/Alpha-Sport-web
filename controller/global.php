b8bf4f983aa3e6c424f04f853257020a04821493
SergioPerez <dark07perez@gmail.com>
2018-11-18 22:26:26 -0500

    Comentado result en global para periodo

--- controller/global.php
+++ controller/global.php
@@ -2,14 +2,14 @@
 # Lugar donde van las variables del proyecto por periodo.
 require 'database.php';
 #agrega el periodo presente
-$query = "SELECT id_periodo, id_nombre FROM tperiodos WHERE id_periodo = '_2018_2_i'";
-$consul = mysqli_query($conn, $query) or die(mysqli_error($conn));
-$results = mysqli_fetch_array($consul); 
+#$query = "SELECT id_periodo, id_nombre FROM tperiodos WHERE id_periodo = '_2018_2_i'";
+#$consul = mysqli_query($conn, $query) or die(mysqli_error($conn));
+#$results = mysqli_fetch_array($consul); 
 
 $MAX_ESTUDIANTES = 350; 
 $MAX_NUEVOS = 175;
 $MAX_ANTIGUOS = 175;
 $MAX_TURNOS_DIA=20;
-$PERIODO= $results['id_periodo'];
-$N_PERIODO= $results['id_nombre'];
+#$PERIODO= "PERIODO DE PRUEBA";
+$N_PERIODO= "PERIODO 2018 - PRUEBA"
 ?>
